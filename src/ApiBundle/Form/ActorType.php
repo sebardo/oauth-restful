152d2f4597d886eb79f612d0748fa6607e313528
Sebardo <sebastian.sasturain@gmail.com>
2017-07-09 13:02:33 +0200

    Add form

---
+++ src/ApiBundle/Form/ActorType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ApiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ActorType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('password')
+            ->add('email')
+
+            ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ApiBundle\Entity\Actor',
+            'csrf_protection' => false,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'apibundle_actor';
+    }
+
+
+}
